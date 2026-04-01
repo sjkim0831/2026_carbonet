68c3387bbf9e71b99bb6fe9292c3ddd34a3e727d
carbosys-ci <ci@carbosys.local>
2026-04-02 01:49:13 +0900

    Use shorter hydrated lime code id

--- src/main/java/egovframework/com/feature/admin/service/impl/EmissionManagementConstants.java
+++ src/main/java/egovframework/com/feature/admin/service/impl/EmissionManagementConstants.java
@@ -4,7 +4,7 @@ final class EmissionManagementConstants {
     static final String CARBONATE_CODE_ID = "EMCARB";
     static final String LIME_TYPE_TIER1_CODE_ID = "EMLIM1";
     static final String LIME_TYPE_TIER23_CODE_ID = "EMLIM2";
-    static final String HYDRATED_LIME_CODE_ID = "EMHYDRYN";
+    static final String HYDRATED_LIME_CODE_ID = "EMHYD";
     static final String FLAG_Y = "Y";
     static final String FLAG_N = "N";
 
